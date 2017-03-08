aa7ace960cbefe6a2c2cdf80de7226b021b73a25
1553450 <1553450@li-h009-0t24>
2017-03-08 09:45:58 -0500

    Merge origin/master
    Conflicts:
    	src/test/WindowGame.java

--- src/test/WindowGame.java
+++ src/test/WindowGame.java
@@ -19,8 +19,7 @@ import test.Mario.State;
  * @author Sami
  */
 public class WindowGame extends BasicGame {
-//test
-            
+
     private int compteur = 0;
     int i = 0;
     private GameContainer container;
