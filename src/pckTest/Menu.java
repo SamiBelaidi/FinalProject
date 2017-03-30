50df88668b947aee9dc0d3f2aa4c53e4680b1d09
Alex <Alex@192.168.2.21>
2017-03-30 14:16:57 -0400

    Menu principal

---
+++ src/pckTest/Menu.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pckTest;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author Alex
+ */
+public class Menu extends BasicGameState{
+    
+    Image background;
+    Image onePlayer;
+    Image twoPlayers;
+    
+    public Menu (int State){
+        
+ 
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        
+        background = new Image("super-mario-bros-menu.png");
+        
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+        
+        background.draw(100,100);
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+    }
+
+    @Override
+    public int getID() {
+        return 0;
+    }
+    
+}
